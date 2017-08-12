767e69254b164a7e754cc0ff34803b8fd5a4b670
Namah <Namah Shrestha>
2017-08-12 10:12:52 -0700

    fixed image to the header

--- js/index_jquery.js
+++ js/index_jquery.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
+	var headerheight = $('.page_header').outerHeight(true);
 	$(this).on("scroll",function(){
-		if($(this).scrollTop()>568){
+		if($(this).scrollTop()>headerheight){
 			$('.clearfix').addClass('sticky');
 		}else{
 			$('.clearfix').removeClass('sticky');
